f10fbf2785790916e76722004088e7afcf41a0c7
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-14 17:44:38 -0400

    added tables for cart container

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import './Cart.css';
 
 const Cart = () => {
   return (
-    <div>
-      {/* <a href="">MY CART</a> */}
+    <div className="cart-page">
+      <center>
+        <h2 className="cart-page-header">Items in Your Cart</h2>
+
+        <div className="cart-container">
+          <table className="cart-items">
+            <thead>Cart</thead>
+            <tr>
+
+            </tr>
+          </table>
+          <table className="cart-summary">
+            <thead align="center">Order Summary</thead>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>$1000.00</td>
+              </tr>
+              <tr>
+              <td>Shipping</td>
+                <td>$1000.00</td>
+              </tr>
+              <tr>
+                <td>Sales Tax</td>
+                <td>$1000.00</td>
+              </tr>
+              <tr>
+              <td>Total</td>
+                <td>$1000.00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </center>
     </div>
   )
 }
